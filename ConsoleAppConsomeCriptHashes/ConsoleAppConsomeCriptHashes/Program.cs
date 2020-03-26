da3737247cb0b62fe3c6e678573aff6f0f163962
mauricio.junior <mauricio.junior@farfetch.com>
2020-03-26 18:00:00 +0000

    add
    add project

---
+++ ConsoleAppConsomeCriptHashes/ConsoleAppConsomeCriptHashes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleAppConsomeCriptHashes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#### Consuming CriptHashes!");
+
+            CriptHashes.Hash hash = new CriptHashes.Hash();
+            
+            Console.WriteLine("my name is Mauricio Junior 512 = " + hash.GetHash("my name is Mauricio Junior", CriptHashes.Enum.HashType.SHA512));
+
+            Console.WriteLine("my name is Mauricio Junior 384 = " + hash.GetHash("my name is Mauricio Junior", CriptHashes.Enum.HashType.SHA384));
+
+            Console.WriteLine("my name is Mauricio Junior 256 = " + hash.GetHash("my name is Mauricio Junior", CriptHashes.Enum.HashType.SHA256));
+
+            Console.WriteLine("my name is Mauricio Junior SHA1 = " + hash.GetHash("my name is Mauricio Junior", CriptHashes.Enum.HashType.SHA1));
+
+            Console.Read();
+        }
+    }
+}
